9a166e0f0048e3d4b7035e874d119b33ef0ef197
Hannah Diehl <hdiehl@mit.edu>
2016-09-17 19:34:42 -0400

    added plurality vote for calculateResults.js

--- routes/calculateResults.js
+++ routes/calculateResults.js
@@ -1,7 +1,45 @@
 module.exports = function(votes, questions) {
 
-    // do something
+    var results=[]
+
+    for (var question in questions){
+    	if (questions.hasOwnProperty(question)){
+    		if (question.type=="majority"){
+	    		var candidateCounts={}
+	    		for (var cand in question.candidates){
+	    			if (question.candidates.hasOwnProperty(cand)) {
+	    				candidateCounts[cand]=0
+	    			}
+	    		}
+	    		for (var vote in votes) {
+	    			if (votes.hasOwnProperty(vote)) {
+	    				choice=vote.questionMap[question]
+	    				candidateCounts[choice]+=1
+	    			}
+	    		}
+	    	}
+	    }
+	    question.candidates.sort(function(a, b) {
+	    	return candidateCounts[a] > candidateCounts[b];
+	    });
+
+	    qResult=[]
+	    for (var cand in question.candidates){
+	    	if (question.candidates.hasOwnProperty(cand)){
+	    		qResult.push([cand, candidateCounts[cand]])
+	    	}
+	    }
+	    results.push(qResult)
+	}
+
+
+
+
+
+
+
+    }
     
-    return data;
+    return results;
     
 };
